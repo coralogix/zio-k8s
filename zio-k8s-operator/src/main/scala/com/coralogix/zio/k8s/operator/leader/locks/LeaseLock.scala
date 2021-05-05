6849e644502e4ca1f0047c7707e8e2703ca985d4
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2021-05-05 12:07:59 +0200

    Lease based leader election

---
+++ zio-k8s-operator/src/main/scala/com/coralogix/zio/k8s/operator/leader/locks/LeaseLock.scala
@@ -0,0 +1,229 @@
+package com.coralogix.zio.k8s.operator.leader.locks
+
+import com.coralogix.zio.k8s.client.K8sFailure
+import com.coralogix.zio.k8s.client.K8sFailure.syntax._
+import com.coralogix.zio.k8s.client.coordination.v1.leases.Leases
+import com.coralogix.zio.k8s.client.model.{ K8sNamespace, Optional }
+import com.coralogix.zio.k8s.model.coordination.v1.{ Lease, LeaseSpec }
+import com.coralogix.zio.k8s.model.core.v1.Pod
+import com.coralogix.zio.k8s.model.pkg.apis.meta.v1.{ MicroTime, ObjectMeta }
+import com.coralogix.zio.k8s.operator.leader
+import com.coralogix.zio.k8s.operator.leader.LeaderElection.logLeaderElectionFailure
+import com.coralogix.zio.k8s.operator.leader._
+import com.coralogix.zio.k8s.operator.leader.locks.LeaseLock.VersionedRecord
+import zio.clock.Clock
+import zio.duration._
+import zio.logging.{ log, Logging }
+import zio.random.Random
+import zio.{ clock, Has, IO, Queue, Ref, Schedule, ZIO, ZManaged }
+
+import java.time.{ DateTimeException, OffsetDateTime }
+
+class LeaseLock(
+  lockName: String,
+  leases: Leases.Service,
+  random: Random.Service,
+  leadershipLost: Queue[Unit],
+  leaseDuration: Duration,
+  renewTimeout: Duration,
+  retryPeriod: Duration
+) extends LeaderLock {
+
+  override def acquireLock(
+    namespace: K8sNamespace,
+    pod: Pod
+  ): ZManaged[Clock with Logging, leader.LeaderElectionFailure[Nothing], Unit] =
+    for {
+      store <- Ref.makeManaged[Option[VersionedRecord]](None)
+      name  <- pod.getName.mapError(KubernetesError).toManaged_
+      impl   = new Impl(store, namespace, name)
+      _     <- impl
+                 .acquire()
+                 .provideSome[Clock with Logging](_ ++ Has(random))
+                 .toManaged(_ => impl.release())
+      _     <- impl.renew().fork.toManaged_
+    } yield ()
+
+  class Impl(
+    store: Ref[Option[VersionedRecord]],
+    namespace: K8sNamespace,
+    identity: String
+  ) {
+    def acquire(): ZIO[Random with Clock with Logging, LeaderElectionFailure[Nothing], Unit] =
+      tryAcquireOrRenew().asSomeError
+        .flatMap {
+          case false => ZIO.fail(None)
+          case true  => ZIO.unit
+        }
+        .zipLeft(log.info("Leadership acquired"))
+        .tapError {
+          case None          => ZIO.unit
+          case Some(failure) =>
+            logLeaderElectionFailure(failure) *>
+              log.info("Failed to take leadership, will retry...")
+        }
+        .retry(Schedule.fixed(retryPeriod).jittered(0.0, 1.2))
+        .optional
+        .unit
+
+    def renew(): ZIO[Clock with Logging, LeaderElectionFailure[Nothing], Unit] = {
+      tryAcquireOrRenew()
+        .retry(
+          (Schedule.fixed(retryPeriod) >>> Schedule.elapsed)
+            .whileOutput(_ < renewTimeout)
+            .as(false)
+        ) // trying to renew within the renew timeout period
+        .repeat(
+          Schedule.fixed(retryPeriod).whileInput((success: Boolean) => success)
+        ) // keep renewing if it succeeded
+    } *> log.info("Leadership lost") *> leadershipLost.offer(()).unit
+
+    def release(): ZIO[Clock with Logging, Nothing, Unit] =
+      store.get
+        .flatMap {
+          case Some(stored) if stored.record.holderIdentity == identity =>
+            for {
+              now      <- clock.currentDateTime.mapError(DateTimeError)
+              newRecord =
+                LeaderElectionRecord(
+                  holderIdentity = identity,
+                  leaseDuration = 1.second,
+                  now,
+                  now,
+                  stored.record.leaderTransitions
+                )
+              _        <- update(newRecord, stored.resourceVersion)
+            } yield ()
+          case _                                                        => ZIO.unit
+        }
+        .catchAll { error =>
+          logLeaderElectionFailure(error)
+        }
+
+    private def tryAcquireOrRenew()
+      : ZIO[Clock with Logging, LeaderElectionFailure[Nothing], Boolean] =
+      for {
+        latest <- get()
+        now    <- clock.currentDateTime.mapError(DateTimeError)
+        result <- latest match {
+                    case None            =>
+                      val record = LeaderElectionRecord(identity, leaseDuration, now, now, 0)
+                      for {
+                        _               <-
+                          log.info(
+                            s"Lease record $lockName does not exist, creating and taking leadership"
+                          )
+                        resourceVersion <- create(record)
+                        _               <- store.set(Some(VersionedRecord(record, resourceVersion, now)))
+                      } yield true
+                    case Some(versioned) =>
+                      for {
+                        _              <- updateStore(versioned)
+                        isLeader        = versioned.record.holderIdentity == identity
+                        canBecomeLeader =
+                          !(versioned.record.renewTime.plus(leaseDuration).isAfter(now))
+                        result         <-
+                          if (!isLeader && !canBecomeLeader) {
+                            ZIO.succeed(false)
+                          } else {
+                            val newRecord =
+                              LeaderElectionRecord(
+                                holderIdentity = identity,
+                                leaseDuration = leaseDuration,
+                                acquireTime = if (isLeader) versioned.record.acquireTime else now,
+                                renewTime = now,
+                                leaderTransitions =
+                                  if (isLeader) versioned.record.leaderTransitions + 1 else 0
+                              )
+                            for {
+                              newResourceVersion <- update(newRecord, versioned.resourceVersion)
+                              updatedRecord       = VersionedRecord(newRecord, newResourceVersion, now)
+                              _                  <- updateStore(updatedRecord)
+                            } yield true
+                          }
+                      } yield result
+                  }
+      } yield result
+
+    private def updateStore(newRecord: VersionedRecord): ZIO[Any, Nothing, Unit] =
+      store.get.flatMap {
+        case None            =>
+          store.set(Some(newRecord))
+        case Some(oldRecord) =>
+          store.set(Some(newRecord)).unless(oldRecord.record == newRecord.record)
+      }
+
+    private def get(): ZIO[Clock, LeaderElectionFailure[Nothing], Option[VersionedRecord]] =
+      (for {
+        lease           <- leases.get(lockName, namespace)
+        record          <- toLeaderElectionRecord(lease)
+        meta            <- lease.getMetadata
+        resourceVersion <- meta.getResourceVersion
+        now             <- clock.currentDateTime.orDie
+      } yield VersionedRecord(record, resourceVersion, now)).ifFound
+        .mapError(KubernetesError.apply)
+        .unrefine { case ex: DateTimeException =>
+          DateTimeError(ex)
+        }
+
+    private def create(record: LeaderElectionRecord): IO[LeaderElectionFailure[Nothing], String] =
+      (for {
+        lease           <- leases.create(toLease(record, None), namespace)
+        leaseMeta       <- lease.getMetadata
+        resourceVersion <- leaseMeta.getResourceVersion
+      } yield resourceVersion).mapError(KubernetesError.apply)
+
+    private def update(
+      record: LeaderElectionRecord,
+      resourceVersion: String
+    ): IO[LeaderElectionFailure[Nothing], String] =
+      (for {
+        lease           <- leases.replace(lockName, toLease(record, Some(resourceVersion)), namespace)
+        leaseMeta       <- lease.getMetadata
+        resourceVersion <- leaseMeta.getResourceVersion
+      } yield resourceVersion).mapError(KubernetesError.apply)
+
+    private def toLeaderElectionRecord(lease: Lease): IO[K8sFailure, LeaderElectionRecord] =
+      for {
+        spec            <- lease.getSpec
+        holderIdentity  <- spec.getHolderIdentity
+        leaseDuration   <- spec.getLeaseDurationSeconds
+        acquireTime     <- spec.getAcquireTime
+        renewTime       <- spec.getRenewTime
+        leaseTransitions = spec.leaseTransitions.getOrElse(0)
+      } yield locks.LeaderElectionRecord(
+        holderIdentity,
+        leaseDuration.seconds,
+        acquireTime.value,
+        renewTime.value,
+        leaseTransitions
+      )
+
+    private def toLease(
+      record: LeaderElectionRecord,
+      resourceVersion: Optional[String]
+    ): Lease =
+      Lease(
+        metadata = ObjectMeta(
+          name = lockName,
+          namespace = namespace.value,
+          resourceVersion = resourceVersion
+        ),
+        spec = LeaseSpec(
+          holderIdentity = record.holderIdentity,
+          leaseDurationSeconds = record.leaseDuration.toSeconds.toInt,
+          acquireTime = MicroTime(record.acquireTime),
+          renewTime = MicroTime(record.renewTime),
+          leaseTransitions = record.leaderTransitions
+        )
+      )
+  }
+}
+
+object LeaseLock {
+  case class VersionedRecord(
+    record: LeaderElectionRecord,
+    resourceVersion: String,
+    observedAt: OffsetDateTime
+  )
+}
--- zio-k8s-operator/src/main/scala/com/coralogix/zio/k8s/operator/leader/package.scala
+++ zio-k8s-operator/src/main/scala/com/coralogix/zio/k8s/operator/leader/package.scala
@@ -1,23 +1,25 @@
 package com.coralogix.zio.k8s.operator
 
 import com.coralogix.zio.k8s.client.K8sFailure
-import com.coralogix.zio.k8s.client.model.K8sNamespace
+import com.coralogix.zio.k8s.client.coordination.v1.leases.Leases
 import com.coralogix.zio.k8s.client.v1.configmaps.ConfigMaps
 import com.coralogix.zio.k8s.client.v1.pods.Pods
 import com.coralogix.zio.k8s.operator.OperatorFailure.k8sFailureToThrowable
 import com.coralogix.zio.k8s.operator.OperatorLogging.logFailure
-import com.coralogix.zio.k8s.operator.leader.locks.{ ConfigMapLock, CustomLeaderLock }
+import com.coralogix.zio.k8s.operator.OperatorLogging.ConvertableToThrowable._
 import com.coralogix.zio.k8s.operator.contextinfo.{ ContextInfo, ContextInfoFailure }
 import com.coralogix.zio.k8s.operator.leader.locks.leaderlockresources.LeaderLockResources
-import izumi.reflect.Tag
+import com.coralogix.zio.k8s.operator.leader.locks.{ ConfigMapLock, CustomLeaderLock, LeaseLock }
 import zio.clock.Clock
-import zio.duration.durationInt
+import zio.duration.{ durationInt, Duration }
 import zio.logging.{ log, LogAnnotation, Logging }
-import zio.{ Cause, Has, Schedule, ZIO, ZLayer, ZManaged }
+import zio.random.Random
+import zio.{ Cause, Has, Queue, Schedule, ZIO, ZLayer, ZManaged }
+
+import java.time.DateTimeException
 
 package object leader {
   type LeaderElection = Has[LeaderElection.Service]
-
   object LeaderElection {
     trait Service {
 
@@ -48,6 +50,30 @@ package object leader {
           managedLock <- lock.acquireLock(namespace, pod)
         } yield managedLock
 
+    }
+
+    class LiveTemporary(
+      contextInfo: ContextInfo.Service,
+      lock: LeaderLock,
+      leadershipLost: Queue[Unit]
+    ) extends Service {
+      override def lease: ZManaged[Clock with Logging, LeaderElectionFailure[Nothing], Unit] =
+        for {
+          namespace   <- contextInfo.namespace.toManaged_.mapError(ContextInfoError)
+          pod         <- contextInfo.pod.toManaged_.mapError(ContextInfoError)
+          managedLock <- lock.acquireLock(namespace, pod)
+        } yield managedLock
+
+      override def runAsLeader[R, E, A](
+        f: ZIO[R, E, A]
+      ): ZIO[R with Clock with Logging, E, Option[A]] =
+        lease
+          .use { _ =>
+            f.bimap(ApplicationError.apply, Some.apply) raceFirst leadershipLost.take.as(None)
+          }
+          .catchAll((failure: LeaderElectionFailure[E]) =>
+            logLeaderElectionFailure(failure).as(None)
+          )
     }
 
     /** Default retry policy for acquiring the lock
@@ -112,7 +138,43 @@ package object leader {
           new CustomLeaderLock(lockName, retryPolicy, deleteLockOnRelease, configmaps)
         )) >>> fromLock
 
-    private def logLeaderElectionFailure[E](
+    /** Lease based leader election implementation
+      *
+      * The leadership is not guaranteed to be held forever, the effect executed in runAsLeader
+      * may be interrupted. It is recommended to retry runAsLeader in these cases to try to reacquire the lease.
+      *
+      * This is a reimplementation of the Go leaderelection package:
+      * https://github.com/kubernetes/client-go/blob/master/tools/leaderelection/leaderelection.go
+      *
+      * @param lockName Name of the lease resource
+      * @param leaseDuration Duration non-leader candidates must wait before acquiring leadership. This is measured
+      *                      against the time of the last observed change.
+      * @param renewTimeout The maximum time a leader is allowed to try to renew its lease before giving up
+      * @param retryPeriod  Retry period for acquiring and renewing the lease
+      */
+    def leaseLock(
+      lockName: String,
+      leaseDuration: Duration = 15.seconds,
+      renewTimeout: Duration = 10.seconds,
+      retryPeriod: Duration = 2.seconds
+    ): ZLayer[Random with ContextInfo with Leases, Nothing, LeaderElection] =
+      (for {
+        selfInfo       <- ZIO.service[ContextInfo.Service]
+        leases         <- ZIO.service[Leases.Service]
+        random         <- ZIO.service[Random.Service]
+        leadershipLost <- Queue.bounded[Unit](1)
+        lock            = new LeaseLock(
+                            lockName,
+                            leases,
+                            random,
+                            leadershipLost,
+                            leaseDuration,
+                            renewTimeout,
+                            retryPeriod
+                          )
+      } yield new LiveTemporary(selfInfo, lock, leadershipLost)).toLayer
+
+    private[leader] def logLeaderElectionFailure[E](
       failure: LeaderElectionFailure[E]
     ): ZIO[Logging, E, Unit] =
       log.locally(LogAnnotation.Name("Leader" :: Nil)) {
@@ -121,6 +183,8 @@ package object leader {
             logFailure("Failed to gather context info", Cause.fail(error))
           case KubernetesError(error)  =>
             logFailure(s"Kubernetes failure", Cause.fail(error))
+          case DateTimeError(error)    =>
+            log.throwable(s"Failed to query time", error)
           case ApplicationError(error) =>
             ZIO.fail(error)
         }
@@ -140,6 +204,9 @@ package object leader {
   /** Failure while calling the Kubernetes API
     */
   final case class KubernetesError(error: K8sFailure) extends LeaderElectionFailure[Nothing]
+
+  /** Failure while calling the Date-Time API */
+  final case class DateTimeError(error: DateTimeException) extends LeaderElectionFailure[Nothing]
 
   /** Inner effect failed
     */
