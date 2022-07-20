51672fa64edfd51bbcca832f9f3a43c8803ffde2
Sandeep Kota <sandeep.kota@oracle.com>
2022-07-20 22:11:22 +0530

    fix issue related to SSL inputStream

--- zio-k8s-client/src/main/scala/com/coralogix/zio/k8s/client/config/package.scala
+++ zio-k8s-client/src/main/scala/com/coralogix/zio/k8s/client/config/package.scala
@@ -3,15 +3,15 @@ package com.coralogix.zio.k8s.client
 import cats.implicits._
 import com.coralogix.zio.k8s.client.model.K8sCluster
 import io.circe.generic.semiauto.deriveDecoder
-import io.circe.{ parser, Decoder }
+import io.circe.{Decoder, parser}
 import sttp.client3.UriContext
 import sttp.model.Uri
 import zio.config._
 import zio.nio.file.Path
 import zio.process.Command
-import zio.{ Layer, RIO, System, Task, ZIO, ZLayer }
+import zio.{Layer, RIO, Scope, Task, ZIO, ZLayer}
 
-import java.io.{ ByteArrayInputStream, File, FileInputStream, InputStream }
+import java.io.{ByteArrayInputStream, File, FileInputStream, InputStream}
 import java.nio.charset.StandardCharsets
 import java.util.Base64
 
@@ -557,17 +557,15 @@ package object config extends Descriptors {
     } yield auth
   }
 
-  private[config] def loadKeyStream(source: KeySource): ZIO[Any, Throwable, InputStream] =
-    ZIO.scoped {
-      ZIO.fromAutoCloseable {
-        source match {
-          case KeySource.FromFile(path)     =>
-            ZIO.attempt(new FileInputStream(path.toFile))
-          case KeySource.FromBase64(base64) =>
-            ZIO.attempt(new ByteArrayInputStream(Base64.getDecoder.decode(base64)))
-          case KeySource.FromString(value)  =>
-            ZIO.attempt(new ByteArrayInputStream(value.getBytes(StandardCharsets.US_ASCII)))
-        }
+  private[config] def loadKeyStream(source: KeySource): ZIO[Scope, Throwable, InputStream] =
+    ZIO.fromAutoCloseable {
+      source match {
+        case KeySource.FromFile(path)     =>
+          ZIO.attempt(new FileInputStream(path.toFile))
+        case KeySource.FromBase64(base64) =>
+          ZIO.attempt(new ByteArrayInputStream(Base64.getDecoder.decode(base64)))
+        case KeySource.FromString(value)  =>
+          ZIO.attempt(new ByteArrayInputStream(value.getBytes(StandardCharsets.US_ASCII)))
       }
     }
 
